a72175027f0fdfc002d7dcb7bc42c4b799955a7a
Ryan McCardle <github@weba.mccardle.nz>
2022-11-29 15:52:31 +0100

    Added  language to irb info check

--- misc/get_irb_info.m
+++ misc/get_irb_info.m
@@ -2,6 +2,7 @@ function [irb_info] = get_irb_info()
 % Prompt user for irb number, gender, age, handedness and create structure
 
     irb_info.irb_number = input('Enter IRB number: ', 's');
+    irb_info.language = input('Enter language (''e'' for English or ''n'' for Norsk): ', 's');
     irb_info.gender = input('Enter gender: ', 's');
     irb_info.age = input('Enter age: ');
     irb_info.handedness = input('Enter handedness (''l'' or ''r''): ', 's');
